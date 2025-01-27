7c8a8a69fb197bd440177fddc0318a464c1ff90c
emp74ark <92691292+emp74ark@users.noreply.github.com>
2025-01-27 09:25:37 +0100

    fix: file paths

--- scripts/html2markdown.js
+++ scripts/html2markdown.js
@@ -3,10 +3,10 @@ const TurndownService = require('turndown');
 
 const turndownService = new TurndownService();
 
-const html = fs.readFileSync('../index.html', 'utf-8');
+const html = fs.readFileSync('index.html', 'utf-8');
 
 turndownService.remove('title');
 
 const markdown = turndownService.turndown(html).split('\n').slice(2).join('\n');
 
-fs.writeFileSync('../andrei_yurkouski.md', markdown);
+fs.writeFileSync('andrei_yurkouski.md', markdown);
